19235dfe45e3ad97260eea0192d0738ce4a52824
Léo-Nils Boissier <leo.nils2000@gmail.com>
2024-04-03 20:14:00 +0200

    Move get_file_lines behind a trait

--- src/file_provider/mod.rs
+++ src/file_provider/mod.rs
@@ -1,27 +1,39 @@
-pub fn get_file_lines(
-    file_path: &str,
-    start_line: usize,
-    end_line: usize,
-) -> Result<String, std::io::Error> {
-    let file = std::fs::read_to_string(file_path)?;
-    let lines: Vec<&str> = file.lines().collect();
+pub trait FileLinesProvider {
+    fn get_file_lines(&self, start_line: usize, end_line: usize) -> Result<String, std::io::Error>;
+}
 
-    if start_line > lines.len() || end_line > lines.len() {
-        return Err(std::io::Error::new(
-            std::io::ErrorKind::InvalidInput,
-            "index out of bounds",
-        ));
+pub struct LocalFileLinesProvider {
+    file_path: String,
+}
+
+impl LocalFileLinesProvider {
+    pub fn new(file_path: String) -> Self {
+        LocalFileLinesProvider { file_path }
     }
+}
 
-    let result = lines
-        .iter()
-        .skip(start_line - 1)
-        .take(end_line - start_line + 1)
-        .map(|line| line.to_string())
-        .collect::<Vec<String>>()
-        .join("\n");
+impl FileLinesProvider for LocalFileLinesProvider {
+    fn get_file_lines(&self, start_line: usize, end_line: usize) -> Result<String, std::io::Error> {
+        let file = std::fs::read_to_string(&self.file_path)?;
+        let lines: Vec<&str> = file.lines().collect();
 
-    Ok(result)
+        if start_line > lines.len() || end_line > lines.len() {
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::InvalidInput,
+                "index out of bounds",
+            ));
+        }
+
+        let result = lines
+            .iter()
+            .skip(start_line - 1)
+            .take(end_line - start_line + 1)
+            .map(|line| line.to_string())
+            .collect::<Vec<String>>()
+            .join("\n");
+
+        Ok(result)
+    }
 }
 
 #[cfg(test)]
@@ -30,13 +42,15 @@ mod tests {
 
     #[test]
     fn test_open_file_absolute_line_1() {
-        let line_1 = get_file_lines("tests/fixtures/my_code.cpp", 1, 1).unwrap();
+        let file = LocalFileLinesProvider::new("tests/fixtures/my_code.cpp".to_string());
+        let line_1 = file.get_file_lines(1, 1).unwrap();
         assert_eq!(line_1, "#include <iostream>");
     }
 
     #[test]
     fn test_open_file_absolute_line_3_to_8() {
-        let line_3_to_8 = get_file_lines("tests/fixtures/my_code.cpp", 3, 8).unwrap();
+        let file = LocalFileLinesProvider::new("tests/fixtures/my_code.cpp".to_string());
+        let line_3_to_8 = file.get_file_lines(3, 8).unwrap();
         assert_eq!(
             line_3_to_8,
             "unsigned long long factorial(int n) {
@@ -50,7 +64,8 @@ mod tests {
 
     #[test]
     fn test_open_non_existent_file() {
-        let result = get_file_lines("tests/fixtures/non_existent_file.cpp", 1, 1);
+        let file = LocalFileLinesProvider::new("tests/fixtures/non_existent_file.cpp".to_string());
+        let result = file.get_file_lines(1, 1);
         assert!(result.is_err());
         assert!(result
             .unwrap_err()
@@ -60,7 +75,8 @@ mod tests {
 
     #[test]
     fn test_open_out_of_bounds_lines() {
-        let result = get_file_lines("tests/fixtures/my_code.cpp", 1, 100);
+        let file = LocalFileLinesProvider::new("tests/fixtures/my_code.cpp".to_string());
+        let result = file.get_file_lines(1, 100);
         assert!(result.is_err());
         assert!(result
             .unwrap_err()
