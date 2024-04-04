b6b825369356cd853c80d4e919845244a6337600
Léo-Nils Boissier <leo.nils2000@gmail.com>
2024-04-05 00:08:08 +0200

    implement Aggregated from section

--- src/aggregation/aggregated.rs
+++ src/aggregation/aggregated.rs
@@ -15,10 +15,31 @@ impl Aggregated {
         self.lines_count += other.lines_count;
         self.covered_lines_count += other.covered_lines_count;
     }
+
+    fn from_section(value: lcov::report::section::Value) -> Self {
+        let lines_count = value.lines.len() as u32;
+        let covered_lines_count = value
+            .lines
+            .iter()
+            .filter(|(_, value)| value.count > 0)
+            .count() as u32;
+
+        Self {
+            lines_count,
+            covered_lines_count,
+        }
+    }
 }
 
 #[cfg(test)]
 mod test {
+    use lcov::report::section::line::{Key as LineKey, Value as LineValue};
+    use lcov::report::section::{Key as SectionKey, Value as SectionValue};
+
+    use crate::test_utils::builders::{FromCount, FromLineNumber, FromStr};
+
+    use super::Aggregated;
+
     #[test]
     fn when_creating_an_aggregate_from_scratch_count_shall_be_0() {
         let aggregated = super::Aggregated::new();
@@ -44,5 +65,61 @@ mod test {
         assert_eq!(aggregated.covered_lines_count, 15);
         assert_eq!(other.lines_count, 20);
         assert_eq!(other.covered_lines_count, 10);
+    }
+
+    #[test]
+    fn when_creating_from_an_empty_section_it_shall_be_0() {
+        let section_value = SectionValue::default();
+
+        let aggregated = Aggregated::from_section(section_value);
+        assert_eq!(aggregated.lines_count, 0);
+        assert_eq!(aggregated.covered_lines_count, 0);
+    }
+
+    #[test]
+    fn when_creating_from_a_section_with_1_line_3_hit_it_shall_be_1_1() {
+        let mut section_value = SectionValue::default();
+
+        section_value
+            .lines
+            .insert(LineKey::from_line_number(1), LineValue::from_count(3));
+
+        let aggregated = Aggregated::from_section(section_value);
+        assert_eq!(aggregated.lines_count, 1);
+        assert_eq!(aggregated.covered_lines_count, 1);
+    }
+
+    #[test]
+    fn when_creating_from_a_section_with_1_line_0_hit_it_shall_be_1_0() {
+        let mut section_value = SectionValue::default();
+
+        section_value
+            .lines
+            .insert(LineKey::from_line_number(1), LineValue::from_count(0));
+
+        let aggregated = Aggregated::from_section(section_value);
+        assert_eq!(aggregated.lines_count, 1);
+        assert_eq!(aggregated.covered_lines_count, 0);
+    }
+
+    #[test]
+    fn when_creating_from_a_section_with_3_lines_2_covered_it_shall_be_3_2() {
+        let mut section_value = SectionValue::default();
+
+        section_value
+            .lines
+            .insert(LineKey::from_line_number(1), LineValue::from_count(0));
+
+        section_value
+            .lines
+            .insert(LineKey::from_line_number(2), LineValue::from_count(3));
+
+        section_value
+            .lines
+            .insert(LineKey::from_line_number(3), LineValue::from_count(2));
+
+        let aggregated = Aggregated::from_section(section_value);
+        assert_eq!(aggregated.lines_count, 3);
+        assert_eq!(aggregated.covered_lines_count, 2);
     }
 }
--- src/test_utils/builders.rs
+++ src/test_utils/builders.rs
@@ -3,12 +3,34 @@ use std::path::PathBuf;
 pub trait FromStr {
     fn from_str(text: &str) -> Self;
 }
+pub trait FromLineNumber {
+    fn from_line_number(line_number: u32) -> Self;
+}
+
+pub trait FromCount {
+    fn from_count(count: u64) -> Self;
+}
 
 impl FromStr for lcov::report::section::Key {
     fn from_str(text: &str) -> Self {
         lcov::report::section::Key {
             source_file: PathBuf::from(text),
             test_name: String::from(""),
+        }
+    }
+}
+
+impl FromLineNumber for lcov::report::section::line::Key {
+    fn from_line_number(line_number: u32) -> Self {
+        lcov::report::section::line::Key { line: line_number }
+    }
+}
+
+impl FromCount for lcov::report::section::line::Value {
+    fn from_count(count: u64) -> Self {
+        lcov::report::section::line::Value {
+            count,
+            ..Default::default()
         }
     }
 }
