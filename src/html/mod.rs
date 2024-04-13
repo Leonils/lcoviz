5aa9ae94812cd7e80ef44713ba5a2c21c3bcb396
Léo-Nils Boissier <leo.nils2000@gmail.com>
2024-04-13 16:25:01 +0200

    Add children to divs

--- src/html/mod.rs
+++ src/html/mod.rs
@@ -4,15 +4,21 @@ trait ToHtml {
 
 struct Div {
     class_names: Vec<String>,
+    children: Vec<Box<dyn ToHtml>>,
 }
 impl Div {
     fn new() -> Self {
         Div {
             class_names: Vec::new(),
+            children: Vec::new(),
         }
     }
     fn with_class(mut self, class: &str) -> Self {
         self.class_names.push(class.to_string());
+        self
+    }
+    fn with_child(mut self, child: impl ToHtml + 'static) -> Self {
+        self.children.push(Box::new(child));
         self
     }
 }
@@ -22,7 +28,12 @@ impl ToHtml for Div {
             0 => String::new(),
             _ => format!(" class=\"{}\"", self.class_names.join(" ")),
         };
-        format!("<div{}></div>", class_attr)
+        let children_html: String = self.children.iter().map(|c| c.to_html()).collect();
+
+        match children_html.len() {
+            0 => format!("<div{} />", class_attr),
+            _ => format!("<div{}>{}</div>", class_attr, children_html),
+        }
     }
 }
 
@@ -33,13 +44,13 @@ mod tests {
     #[test]
     fn empty_div_shall_render() {
         let div = Div::new();
-        assert_eq!(div.to_html(), "<div></div>");
+        assert_eq!(div.to_html(), "<div />");
     }
 
     #[test]
     fn empty_div_with_class_shall_render() {
         let div = Div::new().with_class("my-class");
-        assert_eq!(div.to_html(), "<div class=\"my-class\"></div>");
+        assert_eq!(div.to_html(), "<div class=\"my-class\" />");
     }
 
     #[test]
@@ -47,9 +58,29 @@ mod tests {
         let div = Div::new()
             .with_class("my-class")
             .with_class("my-other-class");
+        assert_eq!(div.to_html(), "<div class=\"my-class my-other-class\" />");
+    }
+
+    #[test]
+    fn div_with_one_child_shall_render() {
+        let div = Div::new()
+            .with_class("my-class")
+            .with_child(Div::new().with_class("child-class"));
         assert_eq!(
             div.to_html(),
-            "<div class=\"my-class my-other-class\"></div>"
+            "<div class=\"my-class\"><div class=\"child-class\" /></div>"
+        );
+    }
+
+    #[test]
+    fn div_with_two_children_shall_render() {
+        let div = Div::new()
+            .with_class("my-class")
+            .with_child(Div::new().with_class("child1-class"))
+            .with_child(Div::new().with_class("child2-class"));
+        assert_eq!(
+            div.to_html(),
+            "<div class=\"my-class\"><div class=\"child1-class\" /><div class=\"child2-class\" /></div>"
         );
     }
 }
